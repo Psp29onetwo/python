ab9c14249f474b9b7e6252d7150a27878a8fce69
Paurav(#PSP) <58593269+Psp29onetwo@users.noreply.github.com>
2019-12-13 20:40:43 +0530

    beginning of the repository

---
+++ ch1&2/lstrip()rstrip().py
@@ -0,0 +1,9 @@
+message1 = "i am paurav   "         #Right strip
+message2 = "   i am paurav"         #Left strip
+message3 = "   i am paurav   "      # strip
+message4 = "   i am paurav "        #Right strip
+print(message1.rstrip())
+print(message2.rstrip())
+print(message1.lstrip())
+print(message2.lstrip())
+
