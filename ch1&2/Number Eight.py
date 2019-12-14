532f07bfa351b3f5a80ee9cbba303b810995004f
Paurav(#PSP) <58593269+Psp29onetwo@users.noreply.github.com>
2019-12-14 18:02:33 +0530

    Add files via upload

---
+++ ch1&2/Number Eight.py
@@ -0,0 +1,6 @@
+''' Representing the number 8 by addition,
+subtraction, divison and multiplcation'''
+print(4 + 4)
+print(int(16 / 2)) # the result wouls be 8.0 because of division but the result is convertes(typecasted) to integer so the result will be 8 only.
+print(4 * 2)
+print(9 - 1)
---
+++ ch1&2/name_case.py
@@ -0,0 +1,20 @@
+'''Personal meassage'''
+person_name = "Eric"
+print("Hello"+person_name+ ",Would u like to study python today?")
+'''Name case'''
+print(person_name.title())
+print(person_name.lower())
+print(person_name.upper())
+'''Famous quote'''
+person_name = "albert einstein"
+print(person_name.title()+" Once said , \"A person who never made a mistake never tried anything new")
+'''Famous quote 2'''
+famous_name = "Albert Einstein"
+famous_person_quote = "Once said , \"A person who never made a mistake never tried anything new"
+print(famous_name, famous_person_quote)
+'''Stripping Names'''
+my_name = "   Paurav   "
+print("\n \"applied new line before name\""+my_name)
+print("\"applies tab before name\" \t "+my_name)
+print(my_name.lstrip() +"\"left strippes name\"")
+print("\"right stripped name\""+my_name.rstrip())
