fa8d7018e07360567cc4fc8b38004163053de0e7
Paurav(#PSP) <58593269+Psp29onetwo@users.noreply.github.com>
2019-12-17 21:41:30 +0530

    Add files via upload

---
+++ ch3/list.py
@@ -0,0 +1,8 @@
+bicycle =  ['trek', 'cannondale', 'redline', 'specialized']
+#List declartaion and initialization
+print(bicycle) #Printing list
+print(bicycle[0])#Pulling out the first element of list
+print(bicycle[0].title())#printing very first element in list in form of title
+'''Indexing the list'''
+print(bicycle[-1])#returning the last element of list
+print(bicycle[-2])#returning the second last element of the list
