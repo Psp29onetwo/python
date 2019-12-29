249ff62817b2bdcc1f501092700d181fdf481e31
Paurav(#PSP) <58593269+Psp29onetwo@users.noreply.github.com>
2019-12-29 16:21:04 +0530

    Add files via upload

---
+++ ch3/ownlist.py
@@ -0,0 +1,8 @@
+fav_vechicle = ['Bus','Car','Motorcycle','Rickshaw','Metro']
+message1 = "I like to travel in"
+message2 = "I want to buy a new"
+print(message1 + " " +fav_vechicle[0])#mesage1 + first element of list
+print(message1 + " " +fav_vechicle[-1])#mesage1 + Last element of list
+print(message1 + " " +fav_vechicle[3])#mesage1 + fourth element of list
+print(message2 + " "+ fav_vechicle[1])#message2 + second element of list
+print(message2 + " "+ fav_vechicle[2])#mesage2 + third element of list
