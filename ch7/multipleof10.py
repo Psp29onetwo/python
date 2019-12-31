fb73b02b790efd7f6a5f6392ce26ef452a01cdd5
Paurav(#PSP) <58593269+Psp29onetwo@users.noreply.github.com>
2019-12-31 22:26:39 +0530

    Add files via upload

---
+++ ch7/multipleof10.py
@@ -0,0 +1,5 @@
+multiple_of_ten = int(input("Enter the number and i will tell you that entered number is multiple of ten or not: "))
+if (multiple_of_ten % 10) == 0:
+    print("Number is multiple of ten")
+else:
+    print("Number is not multiple of ten")
