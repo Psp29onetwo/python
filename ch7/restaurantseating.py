202cce8a319a8e273c9227160c359162bedbd82c
Paurav(#PSP) <58593269+Psp29onetwo@users.noreply.github.com>
2020-01-01 22:46:31 +0530

    Add files via upload

---
+++ ch7/restaurantseating.py
@@ -0,0 +1,5 @@
+people_for_dinner = int(input("Hi, can you pleade let me know that how many people are in their dinner group: "))
+if people_for_dinner > 8:
+    print("Sir, have to wait for a table.")
+else:
+    print("This way sir your table for " + str(people_for_dinner) + " is ready")
