2155f8d522a16dc1a092a2a52d82c32b660a5981
Paurav(#PSP) <pspofficial2912@gmail.com>
2020-03-31 22:27:26 +0530

    admin.py added

--- ch9/admin.py
+++ ch9/admin.py
@@ -6,3 +6,16 @@ of strings like "can add post", "can delete post", "can ban user", and so on.
 Write a method called show_privileges() that lists the administrator’s set of
 privileges. Create an instance of Admin, and call your method.
 '''
+
+class administrator():
+    def __init__(self, privileges):
+        self.privileges = privileges
+    def show_privileges(self):
+        for i in range(len(self.privileges)):
+            print("This Admin have privileges like : " + self.privileges[i])
+        
+
+
+foo = ["Add post", "Delete post", "Ban user"]
+admin = administrator(foo)
+admin.show_privileges()
